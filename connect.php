43294f7b34b0686df4aa7e3bad6f3f2c25e00b33
Allanfatso <a.f.makwembere@wlv.ac.uk>
2023-05-13 19:50:28 +0100

    Linked to database

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials.*/
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'db2107585');
+define('DB_USERNAME', '2108418');
+define('DB_PASSWORD', 'Leslieblantina@1');
+define('DB_NAME', 'db2108418');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
