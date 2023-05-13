e0ed1ccf3f6298028b8de117ee1f479566ad7ebf
Allanfatso <a.f.makwembere@wlv.ac.uk>
2023-05-13 21:21:27 +0100

    database tables

---
+++ tables.sql
@@ -0,0 +1,14 @@
+create table Billadd(
+    Full_name varchar(255),
+    Email varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+create table Card_det(
+    Name varchar(255),
+    Card_num varchar(255),
+    Exp_month varchar(255),
+    CVV varchar(255),
+    Exp_year year
+);
